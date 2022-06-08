f0aba012adfb0538f862b587e1fb2cd7e19a5c0d
vijayasree garapati <vijayasree@deriv.com>
2022-06-08 15:37:22 +0400

    Updates

---
+++ br/join-us/component---src-pages-404-tsx-db88e9b0fda9bc9f249a.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunksinbad_dev=self.webpackChunksinbad_dev||[]).push([[218],{3902:function(e,n,u){u.r(n);var t=u(5007);n.default=function(){return t.default.createElement("h1",null,"404")}}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-db88e9b0fda9bc9f249a.js.map
