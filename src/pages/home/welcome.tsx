0f55caf58c62091934863d63218d53867a63a4ac
NikitK-deriv <nikita.kolesnik@deriv.com>
2022-06-24 16:48:08 +0300

    chore: replace mistake

--- src/pages/home/welcome.tsx
+++ src/pages/home/welcome.tsx
@@ -220,7 +220,7 @@ const data = {
         },
         {
             header: 'Quality assurance',
-            text: 'Whatever we build goes through rigorous testing to make sure its quality and security meet our tough standarts.',
+            text: 'Whatever we build goes through rigorous testing to make sure its quality and security meet our tough standards.',
             icon: (
                 <StaticImage
                     src="../../images/common/homepage/welcome/other-1.png"
