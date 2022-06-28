5395e281cfa7e758f203fbdf6db01ce10d308959
vijayasree garapati <vijayasree@deriv.com>
2022-06-28 15:42:46 +0400

    Updates

---
+++ br/open-dialer/component---src-pages-job-description-data-ts-bae35d35f76f5f941db9.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunksinbad_dev=self.webpackChunksinbad_dev||[]).push([[38],{7366:function(n,s,e){e.r(s),e.d(s,{positions:function(){return i}});var i=e(8286)}}]);
+//# sourceMappingURL=component---src-pages-job-description-data-ts-bae35d35f76f5f941db9.js.map
